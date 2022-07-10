e1d23145cfa6e4fdc6bd45c6e839f2c1d812e2cb
atsuji <atsuji@atsujinoMacBook-Pro.local>
2022-07-10 23:26:17 +0900

    add: spine のコードを途中まで実装

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,11 +5,26 @@ window.onload = () => {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 
-  console.log("spine")
+  console.log('spine')
   console.log(spine)
 
-  console.log("canvas")
+  console.log('canvas')
   console.log(canvas)
+
+  class App implements spine.SpineCanvasApp {
+    private skeleton: spine.Skeleton
+    private state: spine.AnimationState
+
+    // https://github.com/EsotericSoftware/spine-runtimes/blob/4.1/spine-ts/spine-webgl/example/barebones-dragon.html
+    // https://github.com/EsotericSoftware/spine-runtimes/blob/4.1/spine-ts/spine-webgl/example/barebones.html
+    // https://github.com/EsotericSoftware/spine-runtimes/blob/4.1/spine-ts/spine-webgl/example/drag-and-drop.js
+
+    loadAssets = (canvas: spine.SpineCanvas) => {}
+    initialize = (canvas: spine.SpineCanvas) => {}
+    update = (canvas: spine.SpineCanvas, delta: number) => {}
+    render = (canvas: spine.SpineCanvas) => {}
+    error = (canvas: spine.SpineCanvas, errors: spine.StringMap<string>) => {}
+  }
 
   // new spine.SpineCanvas(canvas, {
   //   pathPrefix: 'assets/spine-data/',
